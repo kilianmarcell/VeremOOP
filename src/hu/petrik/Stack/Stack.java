852a45856f93c99e11f49c9ce11e05adac6a5b75
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-11-11 11:04:19 +0100

    Konstruktor

--- src/hu/petrik/Stack/Stack.java
+++ src/hu/petrik/Stack/Stack.java
@@ -1,7 +1,16 @@
 package hu.petrik.Stack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stack<T> {
 
+    private List<T> element;
 
+    public Stack() {
+
+        this.element = new ArrayList<>();
+
+    }
 
 }
